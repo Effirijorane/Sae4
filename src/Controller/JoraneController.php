24ac038c4c2e0abd7bb0860fced1fcfb3096dd1e
effirijorane <jprefida@gmail.com>
2024-01-17 12:47:45 +0100

    loisirs

--- src/Controller/JoraneController.php
+++ src/Controller/JoraneController.php
@@ -15,4 +15,29 @@ class JoraneController extends AbstractController
             'controller_name' => 'JoraneController',
         ]);
     }
+
+
+    #[Route('/cv', name: 'cv')]
+    public function cv(): Response
+    {
+        return $this->render('jorane/cv.html.twig', [
+            'controller_name' => 'JoraneController',
+        ]);
+    }
+
+    #[Route('/Eporfolio', name: 'Eportfolio')]
+    public function Eportfolio(): Response
+    {
+        return $this->render('jorane/Eportfolio.html.twig', [
+            'controller_name' => 'JoraneController',
+        ]);
+    }
+    #[Route('/Leisure', name: 'Leisure')]
+    public function Leisure(): Response
+    {
+        return $this->render('jorane/Leisure.html.twig', [
+            'controller_name' => 'JoraneController',
+        ]);
+    }
 }
+    
